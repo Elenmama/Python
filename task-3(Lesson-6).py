e10ad25d90e7ddbf0bb4723ef6de883782b0df2f
Elenmama <108695640+Elenmama@users.noreply.github.com>
2022-08-20 11:29:48 +0300

    Create task-3(Lesson-6).py

---
+++ task-3(Lesson-6).py
@@ -0,0 +1,19 @@
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return self.name + " " + self.surname
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+a = Position("Sergey", "Mikhailov", "driver", 12, 23)
+print(a.get_full_name())
+print(a.get_total_income())
